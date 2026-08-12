e29680528716b05e7bf61e83f16ad61b75a07f70
KianChilds <94756266+KianChilds@users.noreply.github.com>
2026-08-12 18:21:26 -0500

    Implemented a basic 404 page
    404 page a basic return home and take me back button

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import Link from 'next/link'
+import { useRouter } from 'next/navigation';
+
+export default function NotFound() {
+    const router = useRouter();
+  return (
+    <div className="prose-note mt-6 text-center">
+      <h2>404 | Page Not Found</h2>
+      <p>This page doesn't exist</p>
+      <Link href="/">Return Home</Link> <a onClick={() => router.back()}>Take me back</a> 
+    </div>
+  )
+}
