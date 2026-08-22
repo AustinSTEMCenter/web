ae00f6962232251765b246febf163c247b0404bd
jwilli-asc <271635411+jwilli-asc@users.noreply.github.com>
2026-08-21 23:53:11 -0500

    Update team photos and ordering
    - Replace team image assets with ASC versions
    - Adjust steering committee member ordering

--- lib/data/team.ts
+++ lib/data/team.ts
@@ -20,16 +20,16 @@ export const teamGroups: { group: string; members: TeamMember[] }[] = [
       { name: "Meason Wiley", title: "Program Development & Curriculum", image: "/images/team/meason-wiley.jpg" },
       { name: "Rylee Lippenholz", title: "Administrative Specialist", image: "/images/team/rylee-lippenholz.jpg" },
       { name: "Joseph Williams", title: "Technology Specialist", image: "/images/team/joseph-williams.jpg" },
-      { name: "Zane Lewin", title: "Prototyping & Fabrication", image: "/images/team/zane-lewin.jpg" },
+      { name: "Zane Lewin", title: "Prototyping & Fabrication", image: "/images/team/zane-lewinASC.jpg" },
       { name: "Kian Childs", title: "Tech & Shop Assistant", image: "/images/team/kian-childs.jpg" },
     ],
   },
   {
     group: "Steering Committee",
     members: [
-      { name: "Peter Wang", title: "Co-Founder", image: "/images/team/peter-wang.jpg" },
-      { name: "Evan Marchman", title: "Co-Founder", image: "/images/team/evan-marchman.jpg" },
+      { name: "Peter Wang", title: "Co-Founder", image: "/images/team/peter-wangASC.jpg" },
       { name: "Leah Silen", title: "Executive Director", image: "/images/team/leah-silen.jpg" },
+      { name: "Evan Marchman", title: "Co-Founder", image: "/images/team/evan-marchmanASC.jpg" },
     ],
   },
 ];
